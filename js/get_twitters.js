fcd1cc7ea2eaa0b4997d955c1fc5e06da38255b4
Erika Greco <egreco@penny-arcade.com>
2010-09-01 09:50:23 -0700

    Updated design to run at 1280x720

--- js/get_twitters.js
+++ js/get_twitters.js
@@ -1,22 +1,22 @@
 function twitterPoll() {
-	getTwitters('paxLines', { 
-		id: 'pax_lines', 
-		count: 3, 
-		enableLinks: true, 
-		ignoreReplies: true, 
-		clearContents: true,
-		template: '<p>%text%</p><a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
-	});
-	getTwitters('officialPax', { 
-		id: 'Official_PAX', 
-		count: 6, 
-		enableLinks: true, 
-		ignoreReplies: true, 
-		clearContents: true,
-		template: '<p>%text%</p><a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
-	});
+    getTwitters('paxLines', { 
+        id: 'pax_lines', 
+        count: 2, 
+        enableLinks: true, 
+        ignoreReplies: true, 
+        clearContents: true,
+        template: '<p>%text%</p><a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
+    });
+    getTwitters('officialPax', { 
+        id: 'Official_PAX', 
+        count: 4, 
+        enableLinks: true, 
+        ignoreReplies: true, 
+        clearContents: true,
+        template: '<p>%text%</p><a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
+    });
 }
 
 twitterPoll();
 
-setInterval("twitterPoll()", 1000);
+setInterval("twitterPoll()", 50000);
