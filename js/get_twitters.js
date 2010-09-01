e42cfe74fa95ca8abd4e4d67b610d7a23e5bf157
Erik Karulf <erik@karulf.com>
2010-09-01 09:48:36 -0700

    Changed the application to poll twitter at 1 second intervals

--- js/get_twitters.js
+++ js/get_twitters.js
@@ -1,16 +1,22 @@
-getTwitters('paxLines', { 
-	id: 'pax_lines', 
-	count: 3, 
-	enableLinks: true, 
-	ignoreReplies: true, 
-	clearContents: true,
-	template: '<p>%text%</p><a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
-});
-getTwitters('officialPax', { 
-	id: 'Official_PAX', 
-	count: 6, 
-	enableLinks: true, 
-	ignoreReplies: true, 
-	clearContents: true,
-	template: '<p>%text%</p><a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
-});
+function twitterPoll() {
+	getTwitters('paxLines', { 
+		id: 'pax_lines', 
+		count: 3, 
+		enableLinks: true, 
+		ignoreReplies: true, 
+		clearContents: true,
+		template: '<p>%text%</p><a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
+	});
+	getTwitters('officialPax', { 
+		id: 'Official_PAX', 
+		count: 6, 
+		enableLinks: true, 
+		ignoreReplies: true, 
+		clearContents: true,
+		template: '<p>%text%</p><a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
+	});
+}
+
+twitterPoll();
+
+setInterval("twitterPoll()", 1000);
