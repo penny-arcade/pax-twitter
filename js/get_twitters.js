87a06974caec9eefbc40b49ef7a2e5a50eb89a09
Erik Karulf <erik@karulf.com>
2011-02-11 12:57:05 -0800

    Updated design for PAX East 2011

--- js/get_twitters.js
+++ js/get_twitters.js
@@ -5,7 +5,6 @@ function twitterPoll() {
         enableLinks: true, 
         ignoreReplies: true, 
         clearContents: true,
-        trimUser: true,
         template: '<p>%text%</p><a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
     });
     getTwitters('officialPax', { 
@@ -14,17 +13,10 @@ function twitterPoll() {
         enableLinks: true, 
         ignoreReplies: true, 
         clearContents: true,
-        trimUser: true,
         template: '<p>%text%</p><a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
     });
 }
 
-// The initial request
 twitterPoll();
 
-// We poll *much* faster at the conference (~5 sec)
-// You should only change this if you have a whitelisted ip
-setInterval("twitterPoll()", 5*1000);
-
-// Reload the page once to clear the virtual memory
-setTimeout("window.location.reload()", 30*60*1000);
+setInterval("twitterPoll()", 50000);
