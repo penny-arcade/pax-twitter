aafd455051c769e41a2971ddd74c0f1779966606
Erika Greco <egreco@penny-arcade.com>
2011-02-15 11:22:51 -0800

    The latest two and three column layouts from Erika

--- js/get_twitters.js
+++ js/get_twitters.js
@@ -15,6 +15,14 @@ function twitterPoll() {
         clearContents: true,
         template: '<p>%text%</p><a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
     });
+	getTwitters('paxTourney', { 
+        id: 'pax_tourney', 
+        count: 2, 
+        enableLinks: true, 
+        ignoreReplies: true, 
+        clearContents: true,
+        template: '<p>%text%</p><a class="time" href="http://twitter.com/%user_screen_name%/statuses/%id%/">%time%</a>'
+    });
 }
 
 twitterPoll();
