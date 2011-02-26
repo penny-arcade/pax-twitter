c8732f80a343aa8d8f6ead64cac98a98b1ced7ea
Jon Swope <jaswope@gmail.com>
2011-02-25 21:07:20 -0500

    Make the reconnect timeout work correctly

--- pax_twitter.js
+++ pax_twitter.js
@@ -142,7 +142,7 @@ pax_tweets.addListener('tweet', function(tweet) {
 
 pax_tweets.addListener('end', function(statusCode) {
   console.log("Stream Closed with " + sys.inspect(statusCode));
-  setTimeout(pax_tweets.stream(), 5000);
+  setTimeout(pax_tweets.stream, 5000);
 });
 
 pax_tweets.addListener('error', function(err) {
